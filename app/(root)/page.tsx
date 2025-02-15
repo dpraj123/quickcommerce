02a3706bbc6f724299ac9e870846347bdfcef177
Dharmpal Kumar <rajdp764@gmail.com>
2025-02-15 21:28:20 +0530

    project init

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { ModeToggle } from "@/components/theme/mode-toggle";
+
+export default function Home() {
+  return (
+    <div className=" max-h-screen overflow-y-auto">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat cum
+      doloremque cupiditate voluptatem tempora in unde aut pariatur, iusto
+      nulla, neque similique debitis voluptatum veniam ipsam ut aperiam quam
+      dolorum?
+      <ModeToggle />
+    </div>
+  );
+}
---
+++ lib/dbConfig.ts
@@ -0,0 +1,4 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+export const connection = postgres(process.env.DATABASE_URL as string);
+export const db = drizzle(connection);
